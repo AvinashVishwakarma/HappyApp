4a0ef4cc21b6774cf59a72c76af8d814f7aea5be
Avinash <Avinash@DESKTOP-6ROLNE3>
2018-07-15 20:19:08 +0530

    Add project files.

---
+++ HappyApp/HappyApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HappyApp.Models;
+
+namespace HappyApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
